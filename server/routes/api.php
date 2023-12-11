8a03b409188852f799edfd8be0672d20677e63b4
nhan2211 <220057xa@yse-c.net>
2023-12-11 12:08:57 +0900

    update api

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,18 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\TweetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
+//ルーティングのURL：「/api/xxxx」
+// TweetController
+Route::get('tweet/get', [TweetController::class, 'get']);
+Route::post('tweet/add', [TweetController::class, 'add']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
