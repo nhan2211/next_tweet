6a60a3571243484207cfac3fe8cd787ecefd4230
nhan2211 <220057xa@yse-c.net>
2024-01-16 11:47:49 +0900

    update

--- client/app/user/page.tsx
+++ client/app/user/page.tsx
@@ -1,7 +1,17 @@
+
+import Image from 'next/image';
+import imageMe from '@/public/images/me.png';
+
 const ProfilePage = () => {
     return (
         <div>
-            <h2>Profile</h2>
+            <h1 className="text-3xl text-center font-bold p-3">Profile</h1>
+            <div className="flex justify-center p-3">
+                <Image src={imageMe} className="h-36 w-36 rounded-full" alt="" />
+            </div>
+            <div className="flex justify-center p-3">
+                Dummy User
+            </div>
         </div>
     );
 }
