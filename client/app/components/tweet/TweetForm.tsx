1811e0353add3b539a09022926f6ad6092608f9a
nhan2211 <220057xa@yse-c.net>
2024-01-30 11:54:33 +0900

    update

--- client/app/components/tweet/TweetForm.tsx
+++ client/app/components/tweet/TweetForm.tsx
@@ -1,49 +1,34 @@
-import { User, testUser } from "@/app/models/User";
-import { postTweet } from "@/app/services/TweetService";
-import { useState } from "react";
+"use client"
+
+import { useState } from 'react';
+import ClickButton from '../ClickButton';
 
 interface TweetFormProps {
     onPostTweet: (message: string) => void;
 }
 
+const TweetForm = ({ onPostTweet }: TweetFormProps) => {
+    const [message, setMessage] = useState("");
 
-const TweetForm = ({onPostTweet}: TweetFormProps) => {
-    const [message, setMessage] = useState<string>("")
-    const [isButtonDisabled, setIsButtonDisabled] = useState(true);
-    const enableButtonClassName = `w-full bg-blue-500 hover:bg-blue-700
-                                   text-white font-bold 
-                                   py-3 px-4 mb-2
-                                   rounded`;
-    const disableButtonClassName = `w-full bg-blue-200
-                                   text-white font-bold 
-                                   py-3 px-4 mb-2
-                                   rounded`;
-
-    const messageHandler = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
-        setMessage(event.target.value)
-        setIsButtonDisabled(event.target.value.length == 0);
+    const messageHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setMessage(e.target.value);
     }
 
-    function onPost(): void {
-        onPostTweet(message)
+    const onPost = () => {
+        onPostTweet(message);
         setMessage("");
-        setIsButtonDisabled(true);
     }
+
+    const isDisabled = () => !message;
 
     return (
         <div>
             <textarea
                 value={message}
                 onChange={messageHandler}
-                className="resize-none 
-                    w-full h-24 border rounded-md p-2"
+                className="resize-none w-full h-24 border rounded-md p-2"
                 placeholder="今なにしてる？"></textarea>
-            <div className="p-3">{message.length} characters.</div>
-            <button
-                onClick={onPost}
-                className={isButtonDisabled ? disableButtonClassName : enableButtonClassName}
-                disabled={isButtonDisabled}>
-                Send</button>
+            <ClickButton label="Post" onClick={onPost} disabled={isDisabled()} />
         </div>
     );
 }
